61f9cdec3c0ccd245c962c18ff3f7c987f6f6302
Arjun Singh <arjunkhakh@gmail.com>
2022-03-18 17:52:18 +0000

    adding my connection to the database

--- server.js
+++ server.js
@@ -2,3 +2,13 @@
 const mysql = require('mysql2')
 const inq = require('inquirer')
 const consTab = require('console.table')
+
+// Connection to my Database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: 3000,
+    password: 'arjun',
+    database: 'employee_db'
+});
+
